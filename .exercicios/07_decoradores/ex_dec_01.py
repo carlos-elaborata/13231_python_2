4032c1f2c866daaa421f508b65cd6f8dca8e9f3d
Carlos Cabral <elaborata@cabr.al>
2024-11-29 21:44:31 -0300

    Inicio ex_dec_01

--- .exercicios/07_decoradores/ex_dec_01.py
+++ .exercicios/07_decoradores/ex_dec_01.py
@@ -5,3 +5,28 @@ executar uma função específica.
 Este decorador deve ser aplicado a funções que exigem autenticação prévia para
 acesso.
 """
+
+usuario_autenticado = False
+
+
+def autenticacao_requerida(func):
+    def wrapper(*args, **kwargs):
+        pass
+
+    return wrapper
+
+
+@autenticacao_requerida
+def funcao_protegida() -> None:
+    print("Função protegida executada com sucesso.")
+
+
+# Usuário não está autenticado -> Que a função original não seja executada
+# <mensagem de erro>
+funcao_protegida()
+
+
+usuario_autenticado = True
+# Usuário está autenticado -> Que a função original seja executada
+# "Função protegida executada com sucesso."
+funcao_protegida()
