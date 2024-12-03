31d5f1e35b397842d34029cf8b491256852ca7b3
Carlos Cabral <elaborata@cabr.al>
2024-12-03 19:34:43 -0300

    ex_dataclass_01

--- .exercicios/08_dataclasses/ex_dataclass_01.py
+++ .exercicios/08_dataclasses/ex_dataclass_01.py
@@ -13,7 +13,9 @@ from dataclasses import dataclass, field
 
 @dataclass
 class Produto:
-    pass
+    nome: str
+    preco: float
+    quantidade: int
 
 
 @dataclass
@@ -21,10 +23,41 @@ class Inventario:
     produtos: list[Produto] = field(default_factory=list)
 
     def adicionar_produto(self, produto: Produto) -> None:
-        pass
+        for p in self.produtos:
+            if p.nome == produto.nome:
+                print(f"Produto '{produto.nome}' já existe no inventário.")
+                return
+        self.produtos.append(produto)
+        print(f"Produto '{produto.nome}' adicionado ao inventário.")
 
     def atualizar_estoque(self, nome_produto: str, quantidade: int) -> None:
-        pass
+        for p in self.produtos:
+            if p.nome == nome_produto:
+                p.quantidade += quantidade
+                print(
+                    f"Estoque do produto '{nome_produto}' atualizado para "
+                    f"{p.quantidade}.",
+                )
+                return
+        print(f"Produto '{nome_produto}' não encontrado no inventário.")
 
     def calcular_valor_total(self) -> float:
-        pass
+        valor_total: float = sum(
+            produto.preco * produto.quantidade for produto in self.produtos
+        )
+        print(f"Valor total do inventário: R$ {valor_total:,.2f}")
+        return valor_total
+
+
+inventario = Inventario()
+
+inventario.adicionar_produto(produto=Produto(nome="Camiseta", preco=20, quantidade=50))
+inventario.adicionar_produto(produto=Produto(nome="Camiseta", preco=18, quantidade=55))
+inventario.adicionar_produto(
+    produto=Produto(nome="Calça Jeans", preco=50, quantidade=30),
+)
+
+inventario.atualizar_estoque(nome_produto="Camiseta", quantidade=20)
+inventario.atualizar_estoque(nome_produto="Boné", quantidade=10)
+
+inventario.calcular_valor_total()
