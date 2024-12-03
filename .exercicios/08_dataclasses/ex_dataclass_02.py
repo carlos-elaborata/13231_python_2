85f01bc1bc42227d212bcde3db258f3009f71113
Carlos Cabral <elaborata@cabr.al>
2024-12-02 21:38:26 -0300

    Inicio ex decoradores

--- .exercicios/08_dataclasses/ex_dataclass_02.py
+++ .exercicios/08_dataclasses/ex_dataclass_02.py
@@ -6,3 +6,27 @@ descrição e o status atual (por exemplo, "A Fazer", "Em Andamento" ou "Conclu�
 Implemente uma classe Projeto e métodos para adicionar novas tarefas, alterar o status
 das tarefas e listar todas as tarefas do projeto.
 """
+
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Tarefa:
+    pass
+
+    def __post_init__(self) -> None:
+        pass
+
+
+@dataclass
+class Projeto:
+    tarefas: list[Tarefa] = field(default_factory=list)
+
+    def adicionar_tarefa(self, nova_tarefa: Tarefa) -> None:
+        pass
+
+    def alterar_tarefa(self, nome_tarefa: str, novo_status: str) -> None:
+        pass
+
+    def listar_tarefas(self) -> None:
+        pass
--- .exercicios/08_dataclasses/ex_dataclass_03.py
+++ .exercicios/08_dataclasses/ex_dataclass_03.py
@@ -6,3 +6,27 @@ em diferentes disciplinas.
 Implemente uma classe Escola e métodos para adicionar novos alunos, calcular a média de
 notas de um aluno e listar todos os alunos registrados.
 """
+
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Aluno:
+    nome: str
+    idade: int
+    serie: int
+    notas: dict[str, float]
+
+
+@dataclass
+class Escola:
+    alunos: list[Aluno] = field(default_factory=list)
+
+    def adicionar_aluno(self, aluno: Aluno) -> None:
+        pass
+
+    def calcular_media_notas(self, nome_aluno: str) -> float:
+        pass
+
+    def listar_alunos(self) -> None:
+        pass
