1a6f260dd5d99e57b80ef2892bd245a3878ff645
Carlos Cabral <elaborata@cabr.al>
2024-12-02 21:26:27 -0300

    ex_poo_02

--- .exercicios/06_poo/ex_poo_02.py
+++ .exercicios/06_poo/ex_poo_02.py
@@ -11,3 +11,30 @@ utilizando os métodos implementados.
         2 * 6 = 12
         8 / 2 = 4.0
 """
+
+
+class Calculadora:
+    @staticmethod
+    def adicao(a: float, b: float) -> float:
+        return a + b
+
+    @staticmethod
+    def subtracao(a: float, b: float) -> float:
+        return a - b
+
+    @staticmethod
+    def multiplicacao(a: float, b: float) -> float:
+        return a * b
+
+    @staticmethod
+    def divisao(a: float, b: float) -> float | str:
+        if b != 0:
+            return a / b
+        return "Impossível dividir por zero."
+
+
+print(f"5 + 3 = {Calculadora.adicao(5, 3)}")
+print(f"10 - 4 = {Calculadora.subtracao(10, 4)}")
+print(f"2 * 6 = {Calculadora.multiplicacao(2, 6)}")
+print(f"8 / 2 = {Calculadora.divisao(8, 2)}")
+print(f"5 / 0 = {Calculadora.divisao(5, 0)}")
