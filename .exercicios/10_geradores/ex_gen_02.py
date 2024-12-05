aceaafb137ddcbbe44a01bd21cd7fdca81d0a26d
Carlos Cabral <elaborata@cabr.al>
2024-12-04 21:00:49 -0300

    Inicio ex gen 02

--- .exercicios/10_geradores/ex_gen_02.py
+++ .exercicios/10_geradores/ex_gen_02.py
@@ -7,3 +7,17 @@ e caracteres especiais.
 Em seguida, utilize um loop for para imprimir as primeiras 5 senhas geradas pelo seu
 gerador.
 """
+
+import random
+import string
+
+print(string.ascii_letters)
+print(string.digits)
+print(string.punctuation)
+
+print(
+    random.choices(
+        population=string.ascii_letters + string.digits + string.punctuation,
+        k=10,
+    ),
+)
