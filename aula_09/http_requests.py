d9f8955c54aa805c1a9c3150d1134c81ad6fc80e
Carlos Cabral <elaborata@cabr.al>
2024-12-06 21:03:52 -0300

    aula 10 http requests

--- aula_09/http_requests.py
+++ aula_09/http_requests.py
@@ -12,12 +12,85 @@ if TYPE_CHECKING:
 # for chave, valor in resposta.json().items():
 #     print(f"{chave}: {valor}")
 
-payload: dict[str, str] = {"chave_1": "valor_1", "chave_2": "valor_2"}
-resposta: "Response" = requests.post(
-    url="https://httpbin.org/post",
-    data=payload,
+# payload: dict[str, str] = {"chave_1": "valor_1", "chave_2": "valor_2"}
+# resposta: "Response" = requests.post(
+#     url="https://google.com/",
+#     data=payload,
+#     timeout=10,
+# )
+# print(resposta.text)
+# print(resposta.headers)
+# print(resposta.status_code)
+
+
+# payload: dict[str, str] = {"chave_1": "valor_1", "chave_2": "valor_2"}
+# resposta: "Response" = requests.put(
+#     url="https://httpbin.org/put",
+#     data=payload,
+#     timeout=10,
+# )
+# print(resposta.text)
+# print(resposta.headers)
+# print(resposta.status_code)
+
+
+# payload: dict[str, str] = {"chave_1": "valor_1", "chave_2": "valor_2"}
+# resposta: "Response" = requests.patch(
+#     url="https://httpbin.org/patch",
+#     data=payload,
+#     timeout=10,
+# )
+# print(resposta.text)
+# print(resposta.headers)
+# print(resposta.status_code)
+
+
+# resposta: "Response" = requests.delete(
+#     url="https://httpbin.org/delete",
+#     timeout=10,
+# )
+# print(resposta.text)
+# print(resposta.headers)
+# print(resposta.status_code)
+
+
+# resposta: "Response" = requests.head(
+#     url="https://httpbin.org/get",
+#     timeout=10,
+# )
+# print(resposta.headers)
+# print(resposta.status_code)
+
+
+# resposta: "Response" = requests.options(
+#     url="https://httpbin.org/get",
+#     timeout=10,
+# )
+# print(resposta.headers)
+# print(resposta.status_code)
+
+
+# headers: dict[str, str] = {
+#     "User-Agent": "MeuNavegador/1.0",
+#     "Accept": "application/json",
+#     "Authorization": "Bearer token_de_autenticacao",
+# }
+# resposta: "Response" = requests.get(
+#     url="https://httpbin.org/get",
+#     headers=headers,
+#     timeout=10,
+# )
+# print(resposta.status_code)
+# print(resposta.headers)
+# print(resposta.text)
+
+
+resposta: "Response" = requests.get(
+    url="https://httpbin.org/basic-auth/usuario_batman/senha_abacate",
+    auth=("usuario_batman", "senha_abacate"),
     timeout=10,
 )
-print(resposta.text)
-print(resposta.headers)
+
 print(resposta.status_code)
+print(resposta.headers)
+print(resposta.text)
